03419416b9db0f97d73cc6549d3b92c20eed491e
agnjrgl <agnjr1998@gmail.com>
2017-02-23 10:39:33 -0300

    adicionei a reta

--- Ponto.java
+++ Ponto.java
@@ -17,19 +17,46 @@ public class Ponto{
 		return this.y;
 	}
 	
+	public double distancia(double p1x, double p1y, double p2x, double p2y){
+		double dx=p1x-p2x;	
+		double dy=p1y-p2y;
+		return Math.sqrt(dx*dx+dy*dy);
+	}
+	
+	public Ponto(){
+		System.out.println("Construtor1 " + this);
+	}
+	
+	public Ponto(double x, double y){
+		System.out.println("Construtor2 " + this);
+		this.x=x;
+		this.y=y;
+	}
+	
 	public static void main(String[] args){	
 		System.out.println("Junior");
 		
 		Ponto p1 = new Ponto();
+		p1.setX(2);
+		p1.setY(3);
+
+		Ponto p2 = new Ponto();
+		p1.setX(3);
+		p1.setY(2);
+		
+		System.out.println(p1.distancia(p1.getX(), p1.getY(), p2.getX(), p2.getY()));
+		
+		Reta r1=new Reta();		
+		r1.setP1(p1);
+		r1.setP2(p2);
+			
 		p1.setX(0);
 		p1.setY(0);
-		Ponto p2 = new Ponto();
-		p1.setX(1);
-		p1.setY(1);
-			
-		double dx=p1.getX()-p2.getX();	
-		double dy=p1.getY()-p2.getY();
-		double distancia=Math.sqrt(Math.pow(dx, 2)+Math.pow(dy, 2));
-		System.out.println(distancia);
+		
+		p2.setY(1);
+		p2.setX(1);
+		
+		System.out.println("1 Ponto=> x: "+r1.getP1().getX() +" - y: "+ r1.getP1().getY());
+		System.out.println("2 Ponto=> x: "+r1.getP2().getX() +" - y: "+ r1.getP2().getY()); 
 	}
 }
---
+++ Reta.java
@@ -0,0 +1,19 @@
+public class Reta{
+	private Ponto p1, p2;
+	
+	public void setP1(Ponto p1){
+		this.p1=p1;
+	}
+
+	public void setP2(Ponto p2){
+		this.p2=p2;
+	}
+	
+	public Ponto getP1(){
+		return this.p1;
+	}
+	
+	public Ponto getP2(){
+		return this.p2;
+	}
+}
