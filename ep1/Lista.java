95627886696ff2f3efa67e59004384911fe8b4ba
agnjrgl <agnjr1998@gmail.com>
2017-03-02 11:55:39 -0300

    EP1

---
+++ ep1/Lista.java
@@ -0,0 +1,29 @@
+class No{
+	private:
+		int x;
+		No *prox;
+		No *ant;
+		
+
+}
+
+public class Lista{
+	
+	public
+	
+	public Lista(){
+		
+	}
+	
+	public void Inserir(int valor){
+		
+	}
+	
+	public boolean Pesquisar(int valor){
+		//recebe valor e confere se pertence á lista
+	}
+	
+	public void remover(){
+		//remove o ultimo elemento
+	}
+}
