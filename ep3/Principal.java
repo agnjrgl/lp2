482efecb3cd4b782ec526bff29e358ba59e5bc0c
agnjrgl <agnjr1998@gmail.com>
2017-03-13 12:24:37 -0300

    Resolvi as duas atividades

--- ep3/Principal.java
+++ ep3/Principal.java
@@ -44,13 +44,89 @@ public class Principal{
 		Disciplina disciplina1 = new Disciplina();
 		disciplina1.setNome("LP2");
 		disciplina1.setProfessor("Hebert");
-		disciplinas.add(disciplna1);	
+		disciplinas.add(disciplina1);	
 
 		Disciplina disciplina2 = new Disciplina();
 		disciplina2.setNome("Web");
 		disciplina2.setProfessor("Marcelo");
-		disciplinas.add(disciplna2);	
+		disciplinas.add(disciplina2);	
 		
 		ArrayList<Nota> notas = new ArrayList<Nota>();
+		
+		Nota nota1 = new Nota();
+		nota1.setValor(0.7);
+		nota1.setAluno(aluno1);
+		nota1.setDisciplina(disciplina1);
+		notas.add(nota1);
+		
+		Nota nota2 = new Nota();
+		nota2.setValor(4.6);
+		nota2.setAluno(aluno1);
+		nota2.setDisciplina(disciplina2);
+		notas.add(nota2);
+		
+		Nota nota3 = new Nota();
+		nota3.setValor(8.1);
+		nota3.setAluno(aluno2);
+		nota3.setDisciplina(disciplina1);
+		notas.add(nota3);
+		
+		Nota nota4 = new Nota();
+		nota4.setValor(0.9);
+		nota4.setAluno(aluno2);
+		nota4.setDisciplina(disciplina2);
+		notas.add(nota4);	
+		
+		Nota nota5 = new Nota();
+		nota5.setValor(5.5);
+		nota5.setAluno(aluno3);
+		nota5.setDisciplina(disciplina1);
+		notas.add(nota5);
+		
+		Nota nota6 = new Nota();
+		nota6.setValor(5.7);
+		nota6.setAluno(aluno3);
+		nota6.setDisciplina(disciplina2);
+		notas.add(nota6);		
+
+		Nota nota7 = new Nota();
+		nota7.setValor(3);
+		nota7.setAluno(aluno4);
+		nota7.setDisciplina(disciplina1);
+		notas.add(nota7);
+		
+		Nota nota8 = new Nota();
+		nota8.setValor(8.7);
+		nota8.setAluno(aluno4);
+		nota8.setDisciplina(disciplina2);
+		notas.add(nota8);	
+
+		Nota nota9 = new Nota();
+		nota9.setValor(8.4);
+		nota9.setAluno(aluno5);
+		nota9.setDisciplina(disciplina1);
+		notas.add(nota9);
+		
+		Nota nota10 = new Nota();
+		nota10.setValor(6.2);
+		nota10.setAluno(aluno5);
+		nota10.setDisciplina(disciplina2);
+		notas.add(nota10);	
+		
+		Collections.sort(notas);
+		
+		//Resposta1 Quem tirou maior nota?
+		System.out.println(notas.get(0).getAluno().getNome()+" " +notas.get(0).getValor()+" "+notas.get(0).getDisciplina().getNome());
+		
+		//Resposta2 Maior nota em lp2
+		int aux=2;
+		int i=0;
+		while (aux!=0){
+			if(notas.get(i).getDisciplina().getNome().compareTo("LP2") == 0){
+				System.out.println(notas.get(i).getAluno().getNome()+" "+notas.get(i).getValor());
+				aux=0;
+			}
+			i++;
+		}
 	}
 }
